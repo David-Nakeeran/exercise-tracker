ab42aaed655160c5b2892fa4f521aae768932d37
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-01 10:38:10 +0000

    Added switch statement

--- ExerciseTracker/Coordinators/ApplicationCoordinator.cs
+++ ExerciseTracker/Coordinators/ApplicationCoordinator.cs
@@ -1,11 +1,42 @@
-
+using ExerciseTracker.Utilities;
 
 namespace ExerciseTracker.Coordinators;
 
 class ApplicationCoordinator
 {
+    private readonly UserInput _userInput;
+
+    public ApplicationCoordinator(UserInput userInput)
+    {
+        _userInput = userInput;
+    }
+
     internal void Start()
     {
-        Console.WriteLine("Hello World");
+        bool isAppActive = true;
+
+        while (isAppActive)
+        {
+            var userSelection = _userInput.ShowMenu();
+
+            switch (userSelection)
+            {
+                case "View all exercises":
+
+                    break;
+                case "Create exercise":
+
+                    break;
+                case "Update exercise":
+
+                    break;
+                case "Delete exercise":
+
+                    break;
+                case "Quit application":
+                    isAppActive = false;
+                    break;
+            }
+        }
     }
 }
