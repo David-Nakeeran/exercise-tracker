78a1abe5642eb373cc84240c87f0860673ef1fc3
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-01 13:41:31 +0000

    Added mapper

---
+++ ExerciseTracker/Services/ExerciseMapper.cs
@@ -0,0 +1,17 @@
+using ExerciseTracker.Interface;
+using ExerciseTracker.Models;
+
+namespace ExerciseTracker.Services;
+
+public class ExerciseMapper : IExerciseMapper
+{
+    public ExerciseDTO ExerciseToDTO(Exercise exercise) =>
+        new ExerciseDTO
+        {
+            Id = exercise.Id,
+            DateStart = exercise.DateStart,
+            DateEnd = exercise.DateEnd,
+            Duration = exercise.Duration,
+            Comments = exercise.Comments,
+        };
+}
